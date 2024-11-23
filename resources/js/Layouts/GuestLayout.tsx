f62425c2f1829a32eaf0d7c1956b2a4458cf8692
todd <todd@jupiter>
2024-11-22 18:51:40 -0800

    sort tw classes

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren } from "react";
 
 export default function Guest({ children }: PropsWithChildren) {
   return (
-    <div className="flex min-h-screen flex-row items-start bg-neutral-100 dark:bg-black text-black/50 dark:text-white/50 selection:bg-purple-300 selection:text-black">
+    <div className="flex min-h-screen flex-row items-start bg-neutral-100 text-black/50 selection:bg-purple-300 selection:text-black dark:bg-black dark:text-white/50">
       <div className="max-w-[80ch]">{children}</div>
     </div>
   );
