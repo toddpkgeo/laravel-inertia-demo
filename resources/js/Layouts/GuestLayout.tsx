2688391ebfdf975bd1a5f2ce1e30efd4e46d2a18
todd <todd@jupiter>
2024-11-27 16:06:11 -0800

    simplify GuestLayout
    Note that the "prose" class is usually what sets a decent max width on my articles

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -5,18 +5,16 @@ import { type PropsWithChildren } from "react";
 export default function Guest({ children }: PropsWithChildren) {
   const showHome = usePage().url !== "/";
   return (
-    <div className="flex min-h-screen flex-row items-start bg-black text-neutral-800 selection:bg-purple-300 selection:text-black">
-      <div className="relative flex min-h-screen flex-col items-center">
-        <div className="relative w-full max-w-2xl px-6 py-4 lg:max-w-7xl">
-          <header className="grid py-10">
-            {showHome && (
-              <Link href="/">
-                <ApplicationLogo className="h-10 w-12 fill-current text-neutral-500" />
-              </Link>
-            )}
-          </header>
-          <main className="max-w-[80ch]">{children}</main>
-        </div>
+    <div className="flex min-h-screen flex-row items-start bg-black text-neutral-800">
+      <div className="relative flex min-h-screen w-full flex-col px-6 py-4">
+        <header className="grid pb-6">
+          {showHome && (
+            <Link href="/">
+              <ApplicationLogo className="h-10 w-12 fill-current text-neutral-500" />
+            </Link>
+          )}
+        </header>
+        <main>{children}</main>
       </div>
     </div>
   );
