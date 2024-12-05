b534c74aeb321d9ac6bca92924ae5056b13079dd
todd <todd@jupiter>
2024-12-05 14:33:08 -0800

    add rel=noreferrer to Anchor.tsx

--- resources/js/Components/Anchor.tsx
+++ resources/js/Components/Anchor.tsx
@@ -4,6 +4,7 @@ export default function Anchor({
   className = "",
   href,
   target = "_blank",
+  rel = "noreferrer",
   children,
   ...props
 }: AnchorHTMLAttributes<HTMLAnchorElement>) {
@@ -12,6 +13,7 @@ export default function Anchor({
       {...props}
       href={href}
       target={target}
+      rel={rel}
       className={
         "underline text-blue-600 dark:text-blue-400 hover:text-blue-800 visited:text-purple-600 dark:visited:text-purple-400" +
         className
