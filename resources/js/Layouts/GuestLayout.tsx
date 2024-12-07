dcca85d7e0911503af5e08de23fa730b6f7680e1
todd <todd@jupiter>
2024-12-06 17:02:20 -0800

    fix contrast

--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -5,12 +5,12 @@ import { type PropsWithChildren } from "react";
 export default function Guest({ children }: PropsWithChildren) {
   const showHome = usePage().url !== "/";
   return (
-    <div className="flex min-h-screen flex-row items-start bg-black text-neutral-800">
+    <div className="flex min-h-screen flex-row items-start bg-black text-neutral-300">
       <div className="relative flex min-h-screen w-full flex-col px-6 py-4">
         <header className="grid pb-6">
           {showHome && (
             <Link href="/">
-              <ApplicationLogo className="h-10 w-12 fill-current text-neutral-500" />
+              <ApplicationLogo className="h-10 w-12 fill-current text-neutral-400" />
             </Link>
           )}
         </header>
